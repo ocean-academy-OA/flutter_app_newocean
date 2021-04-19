991977a201442f9fbd840746a7a96432848a8f6c
THAMIZH <thamizharasan2373@gmail.com>
2021-04-19 14:53:46 +0530

    mobile login button added

--- lib/Login/Login_View/Login_responsive.dart
+++ lib/Login/Login_View/Login_responsive.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_newocean/Login/login_widget/new_user_screen/Login/log_in.dart';
+import 'package:flutter_app_newocean/Login/login_widget/new_user_screen/Login/mobile-login.dart';
 
 import 'package:flutter_app_newocean/getx_controller.dart';
 import 'package:get/get.dart';
@@ -17,19 +18,6 @@ class _LoginResponsiveState extends State<LoginResponsive> {
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-      desktop: LogIn(),
-      tablet: LogIn(),
-      mobile: Center(
-        child: Container(
-          color: Colors.yellow,
-          child: TextButton(
-            child: Text('Login Your PC'),
-            onPressed: () {
-              valueController.navebars.value = 'Login';
-            },
-          ),
-        ),
-      ),
-    );
+        desktop: LogIn(), tablet: LogIn(), mobile: MobileLogin());
   }
 }
