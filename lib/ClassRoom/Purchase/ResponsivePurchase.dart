4d9d509d2737ccb1651a142c9169fbc242b39cbb
THAMIZH <thamizharasan2373@gmail.com>
2021-04-22 12:43:46 +0530

    appbar onpress working

---
+++ lib/ClassRoom/Purchase/ResponsivePurchase.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_newocean/ClassRoom/Purchase/desktop_purchase_details.dart';
+import 'package:flutter_app_newocean/ClassRoom/Purchase/mobile_purchase_details.dart';
+import 'package:flutter_app_newocean/ClassRoom/Purchase/tablet_purchase_details.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class ResponsivePurchase extends StatefulWidget {
+  @override
+  _ResponsivePurchaseState createState() => _ResponsivePurchaseState();
+}
+
+class _ResponsivePurchaseState extends State<ResponsivePurchase> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: MobilePurchaseDetails(),
+      tablet: TabletPurchaseDetails(),
+      desktop: DesktopPurchaseDetails(),
+    );
+  }
+}
